f735061e950cb087dbe290143e24c0a30a2d6a81
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2022-02-27 00:05:38 +0700

    update themes; remove un-used themes

--- wp-content/themes/twenty8teen/template-parts/horizontal-rule.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Template part for displaying a horizontal rule
- * @package Twenty8teen
- */
- 
-?>
-	<hr <?php twenty8teen_widget_get_classes( 'horizontal-rule', true ); ?> />
--- wp-content/themes/twenty8teen/template-parts/separator.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Template part for displaying a separator
- * @package Twenty8teen
- */
- 
-?>
-	<div <?php twenty8teen_widget_get_classes( 'separator', true ); ?>>&nbsp;</div>
